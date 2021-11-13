efa66d4f5858d96d73efcbb15bccc3ca8c6583a5
halimyusuf <haleemyoosuph@gmail.com>
2021-11-13 15:38:50 +0100

    style

--- pages/shopping/cart/index.js
+++ pages/shopping/cart/index.js
@@ -3,7 +3,7 @@ import ShoppingCart from "../../../components/shopping.cart";
 
 export default function MainShoppingCart() {
   return (
-    <Box sx={{ width: "450px", maxWidth: "calc(100vw - 65px)" }}>
+    <Box sx={{ width: "350px", maxWidth: "calc(100vw - 65px)" }}>
       <ShoppingCart />
     </Box>
   );
